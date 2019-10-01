df330bd631fae4be684a6fe186db8559dde8d677
Sui <wt@manaosoftware.com>
2019-10-01 11:50:44 +0700

    Add Customer Repository

---
+++ Assignment/Assignment.API/Infrastructure/EF/Repositories/CustomerRepository.cs
@@ -0,0 +1,21 @@
+﻿using Assignment.API.Core.DomainModels;
+using System;
+using System.Threading.Tasks;
+
+namespace Assignment.API.Infrastructure.EF.Repositories {
+
+    public class CustomerRepository : ICustomerRepository {
+
+        public Task<Customer> GetCustomerWithTransactionByEmail(string email) {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerWithTransactionByEmailAndId(string email, int id) {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerWithTransactionById(int id) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assignment/Assignment.API/Infrastructure/EF/Repositories/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using Assignment.API.Core.DomainModels;
+using System.Threading.Tasks;
+
+namespace Assignment.API.Infrastructure.EF.Repositories {
+
+    public interface ICustomerRepository {
+
+        Task<Customer> GetCustomerWithTransactionById(int id);
+        Task<Customer> GetCustomerWithTransactionByEmail(string email);
+        Task<Customer> GetCustomerWithTransactionByEmailAndId(string email, int id);
+    }
+}
