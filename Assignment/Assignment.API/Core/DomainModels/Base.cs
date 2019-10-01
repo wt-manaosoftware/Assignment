6d274207de658c0d415f25942b3662c23cf65f86
Sui <wt@manaosoftware.com>
2019-10-01 10:56:25 +0700

    Add EntityBase model

---
+++ Assignment/Assignment.API/Core/DomainModels/Base.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.API.Core.DomainModels {
+
+    public abstract class EntityBase {
+
+        public long Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
