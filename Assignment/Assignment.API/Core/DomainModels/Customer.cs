74f3abe4bfd13fd36b7841fe401103637223de37
Sui <wt@manaosoftware.com>
2019-10-01 11:02:36 +0700

    Add Customer Model

---
+++ Assignment/Assignment.API/Core/DomainModels/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.API.Core.DomainModels {
+
+    public class Customer : EntityBase {
+
+        public string Name { get; set; }
+        public string ContactEmail { get; set; }
+        public string MobileNo { get; set; }
+    }
+}
