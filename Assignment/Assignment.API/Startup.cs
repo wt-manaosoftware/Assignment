f876e7e21012db447e32046eae0fb06e619aa3d5
Sui <wt@manaosoftware.com>
2019-10-01 13:41:50 +0700

    Add Swagger

--- Assignment/Assignment.API/Startup.cs
+++ Assignment/Assignment.API/Startup.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Assignment.API.Infrastructure.EF;
+﻿using Assignment.API.Infrastructure.EF;
 using Assignment.API.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using StructureMap;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
 
 namespace Assignment.API {
     public class Startup {
@@ -27,6 +22,10 @@ namespace Assignment.API {
         public IServiceProvider ConfigureServices(IServiceCollection services) {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<DBContext>(db => db.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new Info { Title = "Assignment API", Version = "v1" });
+            });
 
             var container = new Container();
             container.Configure(config => {
@@ -47,6 +46,13 @@ namespace Assignment.API {
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+            });
+
         }
     }
 }
