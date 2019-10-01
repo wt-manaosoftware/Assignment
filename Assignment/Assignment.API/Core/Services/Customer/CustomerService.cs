669a9ef1ffc7d8e77456051d5a4c0e38ffaea322
Sui <wt@manaosoftware.com>
2019-10-01 13:54:11 +0700

    Add Customer Service

---
+++ Assignment/Assignment.API/Core/Services/Customer/CustomerService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment.API.Core.DomainModels;
+
+namespace Assignment.API.Infrastructure.EF.Services {
+
+    public class CustomerService : ICustomerService {
+
+        public Task<Customer> GetCustomerByEmailAndIdWithTransaction(string email, int id) {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerByEmailWithTransaction(string email) {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerByIdWithTransaction(int Id) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assignment/Assignment.API/Core/Services/Customer/ICustomerService.cs
@@ -0,0 +1,11 @@
+﻿using Assignment.API.Core.DomainModels;
+using System.Threading.Tasks;
+
+namespace Assignment.API.Infrastructure.EF.Services {
+    public interface ICustomerService {
+
+        Task<Customer> GetCustomerByIdWithTransaction(int Id);
+        Task<Customer> GetCustomerByEmailWithTransaction(string email);
+        Task<Customer> GetCustomerByEmailAndIdWithTransaction(string email, int id);
+    }
+}
