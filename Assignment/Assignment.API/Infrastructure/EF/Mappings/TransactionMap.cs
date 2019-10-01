2a231efe5adb22f48c3da2d0ec08a834ee300cc9
Sui <wt@manaosoftware.com>
2019-10-01 11:18:39 +0700

    Add Transaction Mapping

---
+++ Assignment/Assignment.API/Infrastructure/EF/Mappings/TransactionMap.cs
@@ -0,0 +1,26 @@
+﻿using Assignment.API.Core.DomainModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.API.Infrastructure.EF.Mappings {
+
+    public static class TransactionMap {
+
+        public static ModelBuilder MapTransaction(this ModelBuilder modelBuilder) {
+            var entity = modelBuilder.Entity<Transaction>();
+            entity.ToTable("Transactions");
+            entity.Property(c => c.Id).ValueGeneratedOnAdd();
+            entity.Property(c => c.CurrencyCode);
+            entity.Property(c => c.Amount);
+            entity.Property(c => c.Status);
+            entity.Property(c => c.TransactionDate);
+            entity.Property(c => c.CreatedDate);
+            entity.Property(c => c.UpdatedDate);
+            entity.Property(c => c.CustomerId);
+            return modelBuilder;
+        }
+    }
+}
